40665229e76645d3c1f38faf3b8bd318820e39de
Jingyi Gu <MimoGu@Mimos-MacBook.local>
2018-04-18 16:47:32 -0400

    my first commit

---
+++ myproject/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Post(models.Model):
+
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+	title = models.CharField(max_length=120)
+	# image = models.ImageField(upload_to='records') #records is folder name
+	content = models.TextField()
+	draft = models.BooleanField(default=False)
+	updated = models.DateField(auto_now=True, auto_now_add=False)
+	slug = models.SlugField(max_length=120, unique=True)
+	published = models.DateField(auto_now=False, auto_now_add=True)
+
+	def __str__(self):
+		return "{}, time {}".format(self.title, self.published)
+
+
+
